e9fbf765b75b08c8c30819c864f8de58aa26031b
Hemeline FT <emeline.farget@gmail.com>
2024-06-19 14:47:09 +0200

    Mise en place du projet

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// const stuffRoutes = require('./routes/stuff');
+
+// mongoose.connect('mongodb+srv://emelinefarget:tR6nHzc4LWfiPc4i@coursp7.4ucxtzr.mongodb.net/?retryWrites=true&w=majority&appName=CoursP7',
+//   { useNewUrlParser: true,
+//     useUnifiedTopology: true }) 
+//   .then(() => console.log('Connexion à MongoDB réussie !'))
+//   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// app.use('/api/stuff', stuffRoutes);
+
+module.exports = app;
