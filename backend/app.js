e4793258ef35da92c5e9022b741042abe3139558
Hemeline FT <emeline.farget@gmail.com>
2024-07-22 21:12:23 +0200

    Modifications validation username/password

--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,23 @@
 const express = require('express');
-const { body, validationResult } = require('express-validator');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const bookRoutes = require('./routes/bookRoutes');
 const userRoutes = require('./routes/userRoutes');
+app.use(express.json());
+app.use('/api/auth', userRoutes);
+app.use('/api/books', bookRoutes);
 
 require('dotenv').config();
 console.log(process.env);
 
+//Connexion à MongoDB Atlas
 mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST,
   { useNewUrlParser: true,
     useUnifiedTopology: true }) 
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-
-//On définit une route de validation avec Express validator pour le nom d'utilisateur et le mot de passe
-app.post('/user',
-  //On définit les règles de validation
-  [
-      body('username').isLength({ min: 6 }).withMessage('Le username choisi doit comporter au moins 6 caractères.'),
-      body('email').isEmail().withMessage('Veuillez entrer une adresse email valide.'),
-      body('password').isLength({ min: 6 }).withMessage('Le mot de passe choisi doit comporter au moins 6 caractères.')
-  ],
-  //On crée un middleware qui traite les résultats de la validation
-  (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-      }
-      //En cas de succès, on traite la requête
-      res.send('Utilisateur créé avec succès !');
-  }
-);
-
-
-app.use(express.json());
-app.use(bodyParser.json());
-
 
 //On crée le middleware pour gérer les en-têtes CORS
 app.use((req, res, next) => {
@@ -49,8 +27,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/auth', userRoutes);
-app.use('/api/books', bookRoutes);
-
+function errorHandler ( req, res ) {
+  if (res.headersSent) {
+    return
+  }
+  res.status(404).json({ message: "Le end point demandé n'existe pas."})
+};
+app.use(errorHandler);
 
 module.exports = app;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
-const expressValidator = require('express-validator');
 const userCtrl = require('../controllers/userController');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup',
+      //On définit les règles de validation de Express validator
+      [
+          body('username').isLength({ min: 8 }).withMessage('Le username choisi doit comporter au moins 8 caractères.'),
+          body('email').isEmail().withMessage('Veuillez entrer une adresse email valide.'),
+          body('password').isStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 }).withMessage('Le mot de passe choisi doit comporter au moins 8 caractères, 1 minuscule, 1 majuscule et 1 caractère spécial.')
+      ],
+      //On crée un middleware qui traite les résultats de la validation
+      (req, res, next) => {
+          const errors = validationResult(req);
+          if (!errors.isEmpty()) {
+              return res.status(400).json({ errors: errors.array() });
+          }
+          //En cas de succès, on traite la requête
+          next();
+      },
+        userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 
